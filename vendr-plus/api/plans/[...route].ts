e1efb546dd82aae5e8315749ab713f5e37385db5
Upignata2 <uemersonpignata@yahoo.com.br>
2025-12-18 21:45:46 -0300

    up

---
+++ vendr-plus/api/plans/[...route].ts
@@ -0,0 +1,18 @@
+// @ts-ignore
+import app from '../../server/index.js'
+
+export const config = {
+  api: {
+    bodyParser: false,
+    externalResolver: true,
+  },
+}
+
+export default async (req: any, res: any) => {
+  return new Promise<void>((resolve, reject) => {
+    app(req, res, (err?: any) => {
+      if (err) reject(err)
+      else resolve()
+    })
+  })
+}
