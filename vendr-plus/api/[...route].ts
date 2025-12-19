4800333f3cb9589a10eec63c685ab7106b405b1f
Upignata2 <uemersonpignata@yahoo.com.br>
2025-12-18 22:19:36 -0300

    Fix Vercel handlers - remove externalResolver and use proper VercelRequest/Response types

--- vendr-plus/api/[...route].ts
+++ vendr-plus/api/[...route].ts
@@ -1,14 +1,16 @@
+import { VercelRequest, VercelResponse } from '@vercel/node';
 // @ts-ignore
-import app from '../server/index.js'
+import app from '../server/index.js';
 
 export const config = {
   api: {
     bodyParser: false,
   },
-}
+};
 
-export default (req: any, res: any) => {
-  app(req, res)
-}
+export default (req: VercelRequest, res: VercelResponse) => {
+  app(req, res);
+};
+
 
 
--- vendr-plus/api/pay/[...route].ts
+++ vendr-plus/api/pay/[...route].ts
@@ -1,18 +1,14 @@
+import { VercelRequest, VercelResponse } from '@vercel/node';
 // @ts-ignore
-import app from '../../server/index.js'
+import app from '../../server/index.js';
 
 export const config = {
   api: {
     bodyParser: false,
-    externalResolver: true,
   },
-}
+};
 
-export default async (req: any, res: any) => {
-  return new Promise<void>((resolve, reject) => {
-    app(req, res, (err?: any) => {
-      if (err) reject(err)
-      else resolve()
-    })
-  })
-}
+export default (req: VercelRequest, res: VercelResponse) => {
+  app(req, res);
+};
+
